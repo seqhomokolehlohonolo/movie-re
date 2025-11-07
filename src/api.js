4995e47003d100d4651ab8a56c8186814158126d
seqhomokolehlohonolo <seqhomokolehlohonolo12@gmail.com>
2025-11-06 20:18:29 -0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend URL
+  baseURL: "https://movie-review-0044.onrender.com/api", // your backend URL
 });
 
 export default API;
